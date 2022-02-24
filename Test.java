be282be3db0ec80f0a11a6d91b2c3c3375bb685f
munesh <mk5356708@gmail.com>
2022-02-24 13:29:22 +0500

    this is my first at git hub

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String args[]){
+        for (int i=0; i<=5; i++){
+            for (int j=0; j<i; j++){
+                System.out.println(i);
+
+            }
+
+        }
+    }
+
+}
